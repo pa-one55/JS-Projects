e2fc0920e759a0801d9054b1957cac2e8469c40d
Pa.One <pkpawankumar2806@gmail.com>
2023-05-13 20:35:00 +0530

    added gradient functionality

--- Random Color Generator/js/app.js
+++ Random Color Generator/js/app.js
@@ -4,14 +4,31 @@ const minDecimal = 0;
 const generateSolidColor = () => {
     const color  = Math.floor(Math.random() * (maxDecimal - minDecimal)) + minDecimal;
     const hexCode = "#" + color.toString(16);
-    document.body.style.backgroundColor = hexCode;
+    document.getElementById("solid-box").style.backgroundColor = hexCode;
     document.getElementById("hex-code").innerText = hexCode;
 }
+
+const generateGrad = () => {
+    const color1  = Math.floor(Math.random() * (maxDecimal - minDecimal)) + minDecimal;
+    const hexCode1 = "#" + color1.toString(16);
+    const color2  = Math.floor(Math.random() * (maxDecimal - minDecimal)) + minDecimal;
+    const hexCode2 = "#" + color2.toString(16);
+    const box2 = document.getElementById("grad-color"); 
+    document.getElementById("grad-color").style.background = "linear-gradient(to right, " + hexCode1 + ", " + hexCode2 + ")";
+    document.getElementById("hex-code").innerText = hexCode1 + " , " +  hexCode2 ;
+    console.log(hexCode1, hexCode2);
+}
+
 
 document.getElementById("btn1").addEventListener(
     "click",
     generateSolidColor
 )
 
+document.getElementById("btn2").addEventListener(
+    "click",
+    generateGrad
+)
 
 generateSolidColor();
+generateGrad();
